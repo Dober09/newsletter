d5606396c6316ffcfcd48df60b9b4fe7b407a85b
lelehut022 <lelehut022@student.wethinkcode.co.za>
2023-08-02 20:20:05 +0200

    addad mobile header and section

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import banner from "../assets/images/illustration-sign-up-mobile.svg" 
+export default function Header(){
+    return (
+        <header>
+            <img src={banner} alt="banner" />
+        </header>
+    )
+}
---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+import { data } from "../data"
+import dot from "../assets/images/icon-list.svg"
+export default function Section(){
+    const listOfInfo = data.map(item=>(
+        <div className="list" key={item.id}>
+            <div className="dot"><img src={dot} alt="dot" /></div>
+            <div className="list">{item.msg}</div>
+        </div>
+    ))
+    return(
+        <section>
+            <h2>Stay updated!</h2>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            {listOfInfo}
+        </section>
+    )
+    
+}
